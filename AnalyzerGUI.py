c7eb76dc4c4836d421d32819414f6b0b560865da
ZaharAd <zahar91@gmail.com>
2017-01-10 21:39:33 +0200

    Initial commit

---
+++ AnalyzerGUI.py
@@ -0,0 +1,254 @@
+import tkMessageBox
+from Tkinter import *
+from ttk import *
+from tkFileDialog import askopenfilename
+import matplotlib, numpy, sys
+import matplotlib.ticker as ticker
+import matplotlib.pyplot as pl
+
+matplotlib.use('TkAgg')
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
+
+from DB import *
+from ReadPCAP import *
+
+# Global var
+db = 0
+
+class Gui(Frame):
+    def __init__(self, parent):
+        Frame.__init__(self, parent)
+
+        # init root window
+        self.parent = parent
+        self.parent.title("Network Traffic Analyzer 1.0.0")
+        self.pack(fill=BOTH, expand=1)
+        self.centerWindow()
+
+        # init menu bar
+        self.initMenu()
+
+    def centerWindow(self):
+        w = 1000
+        h = 600
+
+        sw = self.parent.winfo_screenwidth()
+        sh = self.parent.winfo_screenheight()
+
+        x = (sw - w) / 2
+        y = (sh - h) / 2
+        # self.parent.geometry('%dx%d+%d+%d' % (w, h, x, y))
+        self.parent.geometry('%dx%d' % (sw, sh))
+
+    def initMenu(self):
+        menu = Menu(root)
+        root.config(menu=menu)
+        filemenu = Menu(menu)
+        menu.add_cascade(label="File", menu=filemenu)
+        filemenu.add_command(label="Open PCAP File", command=self.openFile)
+        filemenu.add_command(label="Load DB", command=self.loadFile)
+        filemenu.add_separator()
+        filemenu.add_command(label="Exit", command=root.quit)
+
+        helpmenu = Menu(menu)
+        menu.add_cascade(label="Help", menu=helpmenu)
+        helpmenu.add_command(label="About", command=self.about)
+
+    def initBars(self):
+        note = Notebook(root)
+
+        self.tabCnl = Frame(note)
+        self.tabPER = Frame(note)
+        self.tabSsn = Frame(note)
+        self.tabUsr = Frame(note)
+        self.tabCnlPER = Frame(note)
+
+        # add tab and bind to its corresponding graph
+        note.add(self.tabCnl, text="Channel")
+
+        note.add(self.tabPER, text="PER")
+        self.tabPER.bind("<Button-1>", self.showPacketPerUserGraph())
+
+        note.add(self.tabCnlPER, text="Per By Channel")
+        self.tabCnlPER.bind("<Button-1>", self.showPerByChannelGraph())
+
+        note.add(self.tabSsn, text="Sessions")
+
+        note.add(self.tabUsr, text="Users")
+
+        note.place(x=0, y=5)
+
+    def initFrame(self):
+        frame = Frame()
+
+    def loadFile(self):
+        try:
+            fileName = askopenfilename()
+            dbName = fileName[fileName.rindex("/") + 1:fileName.rindex(".")]
+            global db
+            db = DB(dbName)
+            print "loaded file"
+            self.initBars()
+        except:
+            pass
+
+    def openFile(self):
+        try:
+            fileName = askopenfilename()
+            labelfont = ('times', 20, 'bold')
+            loadingMsg = Label(self.parent, text="Loading...")
+            loadingMsg.place(x=450, y=300)
+            loadingMsg.config(font=labelfont)
+            start = time.time()
+            # set DB name and create it
+            dbName = fileName[fileName.rindex("/") + 1:fileName.rindex(".")]
+            global db
+            db = DB(dbName)
+            db.createDB()
+
+            # init and start parse
+            parser = Parser(fileName)
+            parser.startParse()
+            # insert to DB
+            db.insertToTable("usage", parser.getUsageData())
+            db.insertToTable("routers", parser.getRouterData())
+            db.insertToTable("session", parser.getSessionData())
+
+            end = time.time()
+            loadingMsg.destroy()
+            print 'Parsing took %0.3f sec' % (end - start)
+            tkMessageBox.showinfo("Database created",
+                                  "Done loading data.")
+            self.initBars()
+        except Exception as inst:
+            print type(inst)  # the exception instance
+
+            print inst.args  # arguments stored in .args
+
+            print inst  # __str__ allows args to be printed directly
+
+            print "Error creating DB"
+            pass
+
+    def about(self):
+        tkMessageBox.showinfo("About", "This is Network Analyzer used to get data about network and network quality.\n"
+                                       "Version - 1.0.0")
+
+    def showPacketPerUserGraph(self):
+        f = Figure(figsize=(13, 6.5), dpi=100)
+        ax = f.add_subplot(111)
+
+        # Get data from database
+        global db
+        users = db.getUserUsage()
+
+        print (users)
+        macs = []
+        sent = []
+        retransmit = []
+
+        for usr in users:
+            macs.append(usr[0])
+            sent.append(usr[1])
+            retransmit.append(usr[2])
+
+        index = numpy.arange(len(users))  # the x locations for the groups
+        bar_width = .1
+        opacity = 0.4
+
+        ratioList = []
+        for x in range(0,len(retransmit)):
+            if (sent[x] + retransmit[x] != 0) :
+                ratioList.append((sent[x]/(sent[x]+retransmit[x]))*100)
+            else :
+                ratioList.append(100)
+
+
+        ax.bar(index, ratioList, bar_width,
+                        alpha=opacity,
+                        color='m',
+                        label='User Connection Quality')
+
+        # rects2 = ax.bar(index + bar_width, retransmit, bar_width,
+        #                 alpha=opacity,
+        #                 color='r',
+        #                 label='Retransmit')
+
+        ax.legend(loc=1)
+
+        title = "PACKETS PER USER"
+        ax.set_title(title)
+        ax.set_label("mac addres")
+        ax.set_ylabel('Packets PER (%)')
+        ax.set_xticks(index)
+        ax.set_xticklabels(macs, fontsize='small', ha='right', rotation=20)
+        ax.set_xlim([0,20]) #the first 20 macs
+        ax.set_ylim([0, 100])
+
+
+        canvas = FigureCanvasTkAgg(f, master=self.tabPER)
+
+        toolbar = NavigationToolbar2TkAgg(canvas, self.tabPER)
+        toolbar.update()
+        canvas._tkcanvas.pack(side=RIGHT, fill=BOTH, expand=True)
+        canvas.show()
+
+    def showPerByChannelGraph(self):
+        f = Figure(figsize=(13, 6.5), dpi=100)
+        ax = f.add_subplot(111)
+
+        global db
+        channels = db.getChannellUsage()
+
+        channelTab = []
+        packets= []
+        retransmit= []
+
+        for channel in channels:
+            channelTab.append(channel[0])
+            packets.append(channel[1])
+            retransmit.append(channel[2])
+
+        index = numpy.arange(len(channels))
+        bar_width = .1
+        opacity = 0.4
+
+        ratioList = []
+        for x in range(0,len(retransmit)):
+            if (packets[x] + retransmit[x] != 0) :
+                ratioList.append((packets[x]/(packets[x]+retransmit[x]) )*100)
+            else :
+                ratioList.append(100)
+
+
+        print ratioList
+
+        ax.bar(index, ratioList, bar_width,
+                        alpha=opacity,
+                        color='y',
+                        label='Connection Quality')
+
+        ax.legend(loc=1)
+
+        title = "CHANNEL PER"
+        ax.set_title(title)
+        ax.set_xlabel('Channels')
+        ax.set_ylabel('User PER (%)')
+        ax.set_xticks(index)
+        ax.set_xticklabels(channelTab, fontsize='small', ha='right', rotation=20)
+        ax.set_xlim([0,len(channelTab)])
+        ax.set_ylim([0, 100])
+
+        canvas = FigureCanvasTkAgg(f, master=self.tabCnlPER)
+
+        toolbar = NavigationToolbar2TkAgg(canvas, self.tabCnlPER)
+        toolbar.update()
+        canvas._tkcanvas.pack(side=RIGHT, fill=BOTH, expand=True)
+        canvas.show()
+
+
+root = Tk()
+ex = Gui(root)
+
+root.mainloop()
---
+++ DB.py
@@ -0,0 +1,181 @@
+import os
+import sqlite3
+
+
+class DB:
+    def __init__(self, DataBaseName):
+        self.dbName = DataBaseName + ".db"
+
+    def createDB(self):
+        # check if DB exsist -> if so delete
+        if(os.path.isfile(self.dbName)):
+            os.remove(self.dbName)
+        # connect to DB
+        connection = sqlite3.connect(self.dbName)
+
+        cursor = connection.cursor()
+
+        # create Usgae table
+        create_usage_table = """
+        CREATE TABLE usage (
+        MAC VARCHAR(17) PRIMARY KEY,
+        sent DOUBLE,
+        retransmit DOUBLE);"""
+        cursor.execute(create_usage_table)
+
+        # create Session table
+        create_session_table = """
+        CREATE TABLE session (
+        MAC_SRC VARCHAR(17),
+        MAC_DST VARCHAR(17),
+        channel INT,
+        packets DOUBLE,
+        retransmit DOUBLE,
+        PRIMARY KEY (MAC_SRC, MAC_DST, channel));"""
+        cursor.execute(create_session_table)
+
+        # create Routers table
+        create_routers_table = """
+        CREATE TABLE routers (
+        ROUTER VARCHAR(17),
+        CHANNEL INT,
+        SSID VARCHAR(256),
+        CONNECTIONS INTEGER,
+        PRIMARY KEY (ROUTER, CHANNEL));"""
+        cursor.execute(create_routers_table)
+
+        # commit commands and close connection to DB
+        connection.commit()
+        connection.close()
+        print("finish creating DB " + self.dbName)
+
+    def insertToTable(self, tableName, data):
+
+        if (tableName == "usage"):
+            self.__insertToUsageTable(data)
+        elif (tableName == "session"):
+            self.__insertToSessionTable(data)
+        elif (tableName == "routers"):
+            self.__insertToRoutersTable(data)
+        else:
+            print("ERROR: No such table - " + tableName)
+
+    def __insertToUsageTable(self, data):
+
+        # connect to db
+        connection = sqlite3.connect(self.dbName)
+        cursor = connection.cursor()
+
+        for pkts in data.iterkeys():
+            format_str = """INSERT INTO usage (MAC, sent, retransmit)
+            VALUES ("{mac}", "{sent}", "{retransmit}");"""
+
+            sql_command = format_str.format(mac=pkts, sent=data.get(pkts)[0], retransmit=data.get(pkts)[1])
+            cursor.execute(sql_command)
+
+        connection.commit()
+        connection.close()
+
+        print("Done insert to usage table")
+
+    def __insertToSessionTable(self, data):
+        # connect to db
+        connection = sqlite3.connect(self.dbName)
+        cursor = connection.cursor()
+
+        broadcastNum = 0
+        mac_dst = ""
+        for pkts in data.iterkeys():
+            format_str = """INSERT INTO session (MAC_SRC, MAC_DST ,packets, retransmit, channel)
+                    VALUES ("{mac_src}", "{mac_dst}", "{packets}", "{retransmit}", "{channel}");"""
+            # if (pkts[18:35] == "ff:ff:ff:ff:ff:ff"):
+            #     sql_command = format_str.format(mac_src=pkts[:17], mac_dst = broadcastNum, packets=data.get(pkts)[0],
+            #                                     retransmit=data.get(pkts)[1], channel=pkts[36:])
+            #     broadcastNum += 1
+            # else:
+            sql_command = format_str.format(mac_src=pkts[:17], mac_dst=pkts[18:35], packets=data.get(pkts)[0],
+                                                retransmit=data.get(pkts)[1], channel=pkts[36:])
+
+            cursor.execute(sql_command)
+
+        connection.commit()
+        connection.close()
+
+        print("Done insert to session table")
+
+    def __insertToRoutersTable(self, data):
+        # connect to db
+        connection = sqlite3.connect(self.dbName)
+        cursor = connection.cursor()
+
+        for pkts in data.iterkeys():
+            format_str = """INSERT INTO routers (ROUTER, CHANNEL ,SSID, CONNECTIONS)
+                            VALUES ("{router}", "{channel}", "{ssid}", "{connections}");"""
+
+            sql_command = format_str.format(router=pkts[:17], channel=pkts[18:], ssid=data.get(pkts)[0],
+                                            connections=data.get(pkts)[1])
+            cursor.execute(sql_command)
+
+        connection.commit()
+        connection.close()
+
+        print("Done insert to routers table")
+
+
+    # ******************
+    # ***   Quries   ***
+    # ******************
+
+    # run the query at the database and return result
+    def __getQuery(self, query):
+        connection = sqlite3.connect(self.dbName)
+        cursor = connection.cursor()
+        cursor.execute(query)
+        result = cursor.fetchall()
+        connection.close()
+        return result
+
+    # return each user usage
+    # (MAC, packets, retransmit packets)
+    def getUserUsage(self):
+        return self.__getQuery("Select * from usage")
+
+    def getChannellUsage(self):
+        return self.__getQuery("select channel, sum(packets), sum(retransmit) from session group by channel")
+
+
+'''
+#DB run example
+
+rtrs = dict()
+sses = dict()
+usage= dict()
+
+rtrs["FF:FF:FF:FF:FF:FF,4"] = ["toto", 2]
+rtrs["TO:ME:RG:AY:GA:DL,11"] = ["toto2", 3]
+rtrs["TO:ME:RB:LE:AA:AA,3"] = ["toto3", 11]
+
+sses["FF:FF:FF:FF:FF:FF,TT:TT:TT:TT:TT:TT,1"] = [525,72]
+sses["HH:HH:HH:HH:HH:HH,DD:DD:DD:DD:DD:DD,2"] = [285,72]
+sses["GG:GG:GG:Gg:GG:GG,YY:YY:YY:YY:YY:YY,3"] = [16,1]
+
+usage["FF:FF:FF:FF:FF:FF"] = [55,7]
+usage["HH:HH:HH:HH:HH:HH"] = [25,2]
+usage["GG:GG:GG:GG:GG:GG"] = [156,1]
+
+dataTest = DB("test")
+dataTest.createDB()
+
+dataTest.insertToTable("routers", rtrs)
+dataTest.insertToTable("session", sses)
+dataTest.insertToTable("usage", usage)
+
+'''
+
+
+
+
+
+
+
+
